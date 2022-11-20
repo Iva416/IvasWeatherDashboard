3924885aca573e2372eba86070713d649cb20169
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-11-20 11:14:38 -0500

    Added Searchbar

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # IvasWeatherDashboard
+
+### Description
+
+```
+This is a codebase for a web design that shows you the current and future weather conditions for the city of your choice. Simply enter the city in the search bar and hit search.
+```
+
+The following image shows the web application's appearance and functionality:
+
+![Read me pic](README.png 'Read me pic')
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,6 @@
 const cityName = document.querySelector('cityName');
-const SearchBtn = document.querySelector('searchBtn');
-const searchHistory = document.querySelector('searchHistory');
-const cityText = cityName('cityname');
-const Cities = JSON.parse(localStorage.getItem('Cities')) || [];
 
-const addCity = (cityName) => {
-  Cities.push({
-    cityName,
-  });
-  localStorage.setItem('cities', JSON.stringify(Cities));
-  return { cityName };
-};
-
-var Forcast = function (cityName) {
+var fiveForcast = function (cityName) {
   fetch(
     'https://api.openweathermap.org/data/2.5/forecast?q=' +
       cityName +
